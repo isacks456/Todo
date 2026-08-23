515ff6ad956c5016374ca80fbf4757c2b9b400fa
IsmaFx <ismabusiness67@gmail.com>
2026-08-23 21:46:50 +0300

    feat: настроен графический интерфейс Dear ImGui на DirectX 11 и исправлен шрифт

--- Todo.cpp
+++ Todo.cpp
@@ -1,396 +1,288 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <Windows.h>
-#include <fstream>
+// Dear ImGui: standalone example application for Windows API + DirectX 11
 
-class Task
+// Learn about Dear ImGui:
+// - FAQ                  https://dearimgui.com/faq
+// - Getting Started      https://dearimgui.com/getting-started
+// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
+// - Introduction, links and more at the top of imgui.cpp
+
+#include "imgui.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_dx11.h"
+#include <d3d11.h>
+#include <tchar.h>
+
+// Data
+static ID3D11Device*            g_pd3dDevice = nullptr;
+static ID3D11DeviceContext*     g_pd3dDeviceContext = nullptr;
+static IDXGISwapChain*          g_pSwapChain = nullptr;
+static bool                     g_SwapChainOccluded = false;
+static UINT                     g_ResizeWidth = 0, g_ResizeHeight = 0;
+static ID3D11RenderTargetView*  g_mainRenderTargetView = nullptr;
+
+// Forward declarations of helper functions
+bool CreateDeviceD3D(HWND hWnd);
+void CleanupDeviceD3D();
+void CreateRenderTarget();
+void CleanupRenderTarget();
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+// Main code
+int main(int, char**)
 {
-	int id;
-	std::string title;
-	std::string description;
-	bool completed;
+    // Make process DPI aware and obtain main monitor scale
+    ImGui_ImplWin32_EnableDpiAwareness();
+    float main_scale = ImGui_ImplWin32_GetDpiScaleForMonitor(::MonitorFromPoint(POINT{ 0, 0 }, MONITOR_DEFAULTTOPRIMARY));
 
-public:
-	Task(int p_id, std::string p_title, std::string p_description, bool p_completed) : id{ p_id }, title{ std::move(p_title) }, description{std::move(p_description) }, completed{ p_completed }
-	{
+    // Create application window
+    WNDCLASSEXW wc = { sizeof(wc), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr, L"ImGui Example", nullptr };
+    ::RegisterClassExW(&wc);
+    HWND hwnd = ::CreateWindowW(wc.lpszClassName, L"Dear ImGui DirectX11 Example", WS_OVERLAPPEDWINDOW, 100, 100, (int)(1280 * main_scale), (int)(800 * main_scale), nullptr, nullptr, wc.hInstance, nullptr);
 
-	}
+    // Initialize Direct3D
+    if (!CreateDeviceD3D(hwnd))
+    {
+        CleanupDeviceD3D();
+        ::UnregisterClassW(wc.lpszClassName, wc.hInstance);
+        return 1;
+    }
 
-	int GetId() const
-	{
-		return id;
-	}
+    // Show the window
+    ::ShowWindow(hwnd, SW_SHOWDEFAULT);
+    ::UpdateWindow(hwnd);
 
-	const std::string& GetTitle() const
-	{
-		return title;
-	}
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
-	const std::string& GetDescription() const
-	{
-		return description;
-	}
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
 
-	bool GetCompleted() const
-	{
-		return completed;
-	}
+    // Setup scaling
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.ScaleAllSizes(main_scale);        // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
+    style.FontScaleDpi = main_scale;        // Set initial font scale. (in docking branch: using io.ConfigDpiScaleFonts=true automatically overrides this for every window depending on the current monitor)
 
-	void SetTitle(const std::string& newTitle)
-	{
-		title = newTitle;
-	}
+    // Setup Platform/Renderer backends
+    ImGui_ImplWin32_Init(hwnd);
+    ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
 
-	void SetDescription(const std::string& newDescription)
-	{
-		description = newDescription;
-	}
+    // Load Fonts
+    // - If fonts are not explicitly loaded, Dear ImGui will select an embedded font: either AddFontDefaultVector() or AddFontDefaultBitmap().
+    //   This selection is based on (style.FontSizeBase * style.FontScaleMain * style.FontScaleDpi) reaching a small threshold.
+    // - You can load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+    // - If a file cannot be loaded, AddFont functions will return a nullptr. Please handle those errors in your code (e.g. use an assertion, display an error and quit).
+    // - Read 'docs/FONTS.md' for more instructions and details.
+    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use FreeType for higher quality font rendering.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //style.FontSizeBase = 20.0f;
+    //io.Fonts->AddFontDefaultVector();
+    //io.Fonts->AddFontDefaultBitmap();
+    //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf");
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf");
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf");
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf");
+    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf");
 
-	void SetCompeted(const bool& newCompleted)
-	{
-		completed = newCompleted;
-	}
+	ImFontConfig font_config;
+    ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\tahoma.ttf", 18.0f, &font_config, io.Fonts->GetGlyphRangesCyrillic());
+    if (font == nullptr) {
+        io.Fonts->AddFontDefault(); // Если файл шрифта вдруг не найдет, загрузит стандартный англоязычный
+    }
 
-};
+    //IM_ASSERT(font != nullptr);
 
-class Todo
+    // Our state
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    // Main loop
+    bool done = false;
+    while (!done)
+    {
+        // Poll and handle messages (inputs, window resize, etc.)
+        // See the WndProc() function below for our to dispatch events to the Win32 backend.
+        MSG msg;
+        while (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE))
+        {
+            ::TranslateMessage(&msg);
+            ::DispatchMessage(&msg);
+            if (msg.message == WM_QUIT)
+                done = true;
+        }
+        if (done)
+            break;
+
+        // Handle window being minimized or screen locked
+        if (g_SwapChainOccluded && g_pSwapChain->Present(0, DXGI_PRESENT_TEST) == DXGI_STATUS_OCCLUDED)
+        {
+            ::Sleep(10);
+            continue;
+        }
+        g_SwapChainOccluded = false;
+
+        // Handle window resize (we don't resize directly in the WM_SIZE handler)
+        if (g_ResizeWidth != 0 && g_ResizeHeight != 0)
+        {
+            CleanupRenderTarget();
+            g_pSwapChain->ResizeBuffers(0, g_ResizeWidth, g_ResizeHeight, DXGI_FORMAT_UNKNOWN, 0);
+            g_ResizeWidth = g_ResizeHeight = 0;
+            CreateRenderTarget();
+        }
+
+        // Start the Dear ImGui frame
+        ImGui_ImplDX11_NewFrame();
+        ImGui_ImplWin32_NewFrame();
+        ImGui::NewFrame();
+
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+       // if (show_demo_window)
+            //ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Мой Списочек Задач");
+
+			ImGui::Text("Здесь будут отображаться ваши задачи.");
+
+			if (ImGui::Button("Добавить тестовую задачу")) {
+
+			}
+
+			ImGui::End();
+
+
+
+        }
+
+        // 3. Show another simple window.
+        // if (show_another_window)
+        // {
+        //     ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+        //     ImGui::Text("Hello from another window!");
+        //     if (ImGui::Button("Close Me"))
+        //         show_another_window = false;
+        //     ImGui::End();
+        // }
+
+        // Rendering
+        ImGui::Render();
+        const float clear_color_with_alpha[4] = { clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w };
+        g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, nullptr);
+        g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, clear_color_with_alpha);
+        ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+
+        // Present
+        HRESULT hr = g_pSwapChain->Present(1, 0);   // Present with vsync
+        //HRESULT hr = g_pSwapChain->Present(0, 0); // Present without vsync
+        g_SwapChainOccluded = (hr == DXGI_STATUS_OCCLUDED);
+    }
+
+    // Cleanup
+    ImGui_ImplDX11_Shutdown();
+    ImGui_ImplWin32_Shutdown();
+    ImGui::DestroyContext();
+
+    CleanupDeviceD3D();
+    ::DestroyWindow(hwnd);
+    ::UnregisterClassW(wc.lpszClassName, wc.hInstance);
+
+    return 0;
+}
+
+// Helper functions
+
+bool CreateDeviceD3D(HWND hWnd)
 {
-	std::vector<Task> tasks;
-	int newId = 1;
-
-public:
-
-	void addTask()
-	{
-
-		std::cin.ignore(32767, '\n');
-
-		std::string newTitle;
-		std::string newDescription;
-
-		std::cout << "Введите название задачи: ";
-		std::getline(std::cin, newTitle);
-
-
-		std::cout << "Введите описание задачи: ";
-		std::getline(std::cin, newDescription);
-
-
-		tasks.push_back(Task(newId, newTitle, newDescription, false));
-
-		newId++;
-
-
-		std::cout << "Задача успешно добавлена!";
-		std::cout << std::endl;
-
-	}
-
-	void showAllTasks()
-	{
-		std::cout << "Список всех задач: " << std::endl;
-
-		for (size_t i = 0; i < tasks.size(); ++i)
-		{
-			std::cout << "ID: " << tasks[i].GetId() << "\n"
-				<< "Название: " << tasks[i].GetTitle() << "\n"
-				<< "Описание: " << tasks[i].GetDescription() << "\n"
-				<< "Статус: " << (tasks[i].GetCompleted() ? " Выполнено " : " В процессе... ")
-				<< std::endl;
-			
-			std::cout << std::endl;
-		}
-	}
-
-	void showCompletedTasks()
-	{
-
-		std::cout << "Выполненные задачи: " << "\n";
-		bool hasCompleted = false;
-
-		for (size_t i = 0; i < tasks.size(); ++i)
-		{
-			if (tasks[i].GetCompleted())
-			{
-
-				hasCompleted = true;
-
-				std::cout << "ID: " << tasks[i].GetId() << "\n"
-					<< "Название: " << tasks[i].GetTitle() << "\n"
-					<< "Описание: " << tasks[i].GetDescription() << "\n"
-					<< std::endl;
-			}
-
-			if (!hasCompleted)
-			{
-				std::cout << "У вас пока нет выполненных задач" << std::endl;
-			}
-		}
-	}
-
-	void showIsNotCompletedTasks()
-	{
-		std::cout << "Не выполненные задачи: " << std::endl;
-		bool isNotHasCompleted = false;
-
-		for (size_t i = 0; i < tasks.size(); ++i)
-		{
-			if (tasks[i].GetCompleted() == false)
-			{
-
-				isNotHasCompleted = true;
-
-				std::cout << "ID: " << tasks[i].GetId() << "\n"
-					<< "Название: " << tasks[i].GetTitle() << "\n"
-					<< "Описание: " << tasks[i].GetDescription() << "\n"
-					<< std::endl;
-			}
-
-			if (!isNotHasCompleted)
-			{
-				std::cout << "У вас нет невыполненных задач" << std::endl;
-			}
-		}
-	}
-
-	void changeTask()
-	{
-
-		int number;
-
-		bool isFound = false;
-
-		std::cout << "Напишите ID задачи, которую хотите изменить: ";
-		std::cin >> number;
-
-		for (size_t i = 0; i < tasks.size(); ++i)
-		{
-			if (tasks[i].GetId() == number)
-			{
-				isFound = true;
-
-				std::cin.ignore(32767, '\n');
-
-				std::string newTitle;
-				std::string newDescription;
-
-				std::cout << "Введите новый заголовок: ";
-				std::getline(std::cin, newTitle);
-
-				std::cout << "Введите новое описание: ";
-				std::getline(std::cin, newDescription);
-
-				tasks[i].SetTitle(newTitle);
-				tasks[i].SetDescription(newDescription);
-
-				std::cout << "Задача успешно изменена!" << std::endl;
-				break;
-			}
-		}
-
-		if (!isFound)
-		{
-			std::cout << "Вы ввели несуществующее ID!" << std::endl;
-		}
-
-	}
-
-	void taskCompleted()
-	{
-		int number;
-		std::cout << "Введите ID задачи: ";
-		std::cin >> number;
-		bool isFound = false;
-
-		for (size_t i = 0; i < tasks.size(); ++i)
-		{
-			if (tasks[i].GetId() == number)
-			{
-				isFound = true;
-
-				tasks[i].SetCompeted(true);
-
-				std::cout << "Ваша задача выполнена!" << std::endl;
-				break;
-			}
-		}
-
-		if (!isFound)
-		{
-			std::cout << "Вы ввели несуществующее ID!" << std::endl;
-		}
-
-	}
-
-	void dropTask()
-	{
-		int number;
-		int newId = 1;
-		std::cout << "Введите ID задачи: ";
-		std::cin >> number;
-
-		bool isFound = false;
-
-		for (size_t i = 0; i < tasks.size(); ++i)
-		{
-			if (tasks[i].GetId() == number)
-			{
-				isFound = true;
-
-				tasks.erase(tasks.begin() + i);
-
-				std::cout << "Ваша задача успешно удалена!" << std::endl;
-
-				break;
-			}
-		}
-
-		if (!isFound)
-		{
-			std::cout << "Вы ввели несуществующее ID!" << std::endl;
-		}
-
-	}
-
-	void findTask()
-	{
-		int number;
-		std::cout << "ВВедите ID задачи: ";
-		std::cin >> number;
-
-		bool isFound = false;
-
-		for (size_t i = 0; i < tasks.size(); ++i)
-		{
-
-			if (tasks[i].GetId() == number)
-			{
-				isFound = true;
-
-				std::cout << "Ваша задача: " << std::endl;
-				std::cout << "ID: " << tasks[i].GetId() << "\n"
-					<< "Название: " << tasks[i].GetTitle() << "\n"
-					<< "Описание: " << tasks[i].GetDescription() << "\n"
-					<< "Статус: " << (tasks[i].GetCompleted() ? " Выполнено " : " В процессе... ")
-					<< std::endl;
-				
-				break;
-
-			}
-		}
-
-		if (!isFound)
-		{
-			std::cout << "Вы ввели несуществующее ID!" << std::endl;
-		}
-
-	}
-
-	void addToFile()
-	{
-		std::ofstream out("Todo.txt");
-
-		if (!out.is_open())
-		{
-			std::cout << "Ошибка открытия файла на запись!" << std::endl;
-			return;
-		}
-
-		for (size_t i = 0; i < tasks.size(); ++i)
-		{
-				out << tasks[i].GetId() << "\n"
-					<< tasks[i].GetTitle() << "\n"
-					<< tasks[i].GetDescription() << "\n"
-					<< (tasks[i].GetCompleted() ? " Выполнено " : " В процессе... ") << "\n"
-					<< std::endl;
-		}
-
-		std::cout << "Файл успешно создан!" << std::endl;
-
-		out.close();
-
-	}
-
-
-
-};
-
-int main()
+    // Setup swap chain
+    // This is a basic setup. Optimally could use e.g. DXGI_SWAP_EFFECT_FLIP_DISCARD and handle fullscreen mode differently. See #8979 for suggestions.
+    DXGI_SWAP_CHAIN_DESC sd;
+    ZeroMemory(&sd, sizeof(sd));
+    sd.BufferCount = 2;
+    sd.BufferDesc.Width = 0;
+    sd.BufferDesc.Height = 0;
+    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    sd.BufferDesc.RefreshRate.Numerator = 60;
+    sd.BufferDesc.RefreshRate.Denominator = 1;
+    sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    sd.OutputWindow = hWnd;
+    sd.SampleDesc.Count = 1;
+    sd.SampleDesc.Quality = 0;
+    sd.Windowed = TRUE;
+    sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+    UINT createDeviceFlags = 0;
+    //createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+    D3D_FEATURE_LEVEL featureLevel;
+    const D3D_FEATURE_LEVEL featureLevelArray[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_0, };
+    HRESULT res = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext);
+    if (res == DXGI_ERROR_UNSUPPORTED) // Try high-performance WARP software driver if hardware is not available.
+        res = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_WARP, nullptr, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext);
+    if (res != S_OK)
+        return false;
+
+    CreateRenderTarget();
+    return true;
+}
+
+void CleanupDeviceD3D()
 {
-	SetConsoleCP(65001);
-	SetConsoleOutputCP(65001);
-	setlocale(LC_ALL, "ru_RU.UTF-8");
+    CleanupRenderTarget();
+    if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = nullptr; }
+    if (g_pd3dDeviceContext) { g_pd3dDeviceContext->Release(); g_pd3dDeviceContext = nullptr; }
+    if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = nullptr; }
+}
 
-	std::cout << "Привет! Это Список дел!";
+void CreateRenderTarget()
+{
+    ID3D11Texture2D* pBackBuffer;
+    g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+    g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_mainRenderTargetView);
+    pBackBuffer->Release();
+}
 
-	std::cout << "Нужно просто написать цифру для выбора" << std::endl;
+void CleanupRenderTarget()
+{
+    if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = nullptr; }
+}
 
-	int num;
+// Forward declare message handler from imgui_impl_win32.cpp
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-	Todo list;
+// Win32 message handler
+// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+        return true;
 
-	while (true)
-	{
-		std::cout << "1. Добавить задачу \n"
-			<< "2. Показать все задачи \n"
-			<< "3. Показать выполненные задачи \n"
-			<< "4. показать невыполненные задачи \n"
-			<< "5. Изменить задачу \n"
-			<< "6. Отметить задачу выполненной \n"
-			<< "7. Удалить задачу \n"
-			<< "8. Найти задачу \n"
-			<< "9. Создать файл \n"
-			<< "10. Выйти \n"
-			<< std::endl;
-
-		std::cin >> num;
-
-		switch (num)
-		{
-		case 1:
-			list.addTask();
-			std::cout << std::endl;
-			break;
-		case 2:
-			list.showAllTasks();
-			std::cout << std::endl;
-			break;
-		case 3:
-			list.showCompletedTasks();
-			std::cout << std::endl;
-			break;
-		case 4:
-			list.showIsNotCompletedTasks();
-			std::cout << std::endl;
-			break;
-		case 5:
-			list.changeTask();
-			std::cout << std::endl;
-			break;
-		case 6:
-			list.taskCompleted();
-			std::cout << std::endl;
-			break;
-		case 7:
-			list.dropTask();
-			std::cout << std::endl;
-			break;
-		case 8:
-			list.findTask();
-			std::cout << std::endl;
-			break;
-		case 9:
-			list.addToFile();
-			std::cout << std::endl;
-			break;
-		case 10:
-			std::cout << "До свидания!" << std::endl;
-			std::cout << std::endl;
-			return 0;
-		default:
-			std::cout << "Неверный пункт попробуйте еще раз!" << std::endl;
-		}
-
-	}
+    switch (msg)
+    {
+    case WM_SIZE:
+        if (wParam == SIZE_MINIMIZED)
+            return 0;
+        g_ResizeWidth = (UINT)LOWORD(lParam); // Queue resize
+        g_ResizeHeight = (UINT)HIWORD(lParam);
+        return 0;
+    case WM_SYSCOMMAND:
+        if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+            return 0;
+        break;
+    case WM_DESTROY:
+        ::PostQuitMessage(0);
+        return 0;
+    }
+    return ::DefWindowProcW(hWnd, msg, wParam, lParam);
 }
